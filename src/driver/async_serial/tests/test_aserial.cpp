b304189b4d4ff8206f7716f8b46f3316619bc738
Ruixiang Du <ruixiang.du@gmail.com>
2019-06-04 08:03:20 -0400

    tested async serial

--- src/driver/async_serial/tests/test_aserial.cpp
+++ src/driver/async_serial/tests/test_aserial.cpp
@@ -47,14 +47,18 @@ void parse_buffer(uint8_t *buf, const size_t bufsize, size_t bytes_received)
 int main(int argc, char *argv[])
 {
     // ASyncSerial::Ptr serial = ASyncSerial::open_url("/dev/ttyUSB0:115200");
-    std::shared_ptr<ASyncSerial> serial = std::make_shared<ASyncSerial>("/dev/ttyUSB0", 115200);
+    std::shared_ptr<ASyncSerial> serial = std::make_shared<ASyncSerial>("/dev/pts/6", 115200);
 
     serial->set_receive_callback(parse_buffer);
 
     if (serial->is_open())
         std::cout << "serial port opened" << std::endl;
 
+    uint8_t data[8] = {'a','b','c'};
+
     while (1)
     {
+        serial->send_bytes(data, 3);
+        sleep(1);
     }
 }
